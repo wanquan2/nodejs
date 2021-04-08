97b5cb663e0d5b008b62525bb90a3565181d009e
wanquan <615861192@qq.com>
2021-04-08 18:44:42 +0800

    版本1.0.0

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 let http = require("http")
+let fs = require("fs")
 
-let serve = http.createServer(function(requset,response){
-    response.writeHead(200,{'Content-Type': 'text/plain',})
-    response.end('你好')
-}).listen(8080)
+let serve = http.createServer();
+
+serve.on("request",function(request,response){
+    let url = request.url;
+    console.log(url);
+    fs.readFile("./web/index.html",function(err,data){
+        response.writeHead(200,{'Content-Type': 'text/html','token':'wwwwwww-11111'})
+        response.end(data)
+    })
+})
+
+serve.listen(8080,function(){
+    console.log(`请访问localhost:8080`)
+})
