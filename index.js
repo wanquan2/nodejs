aa54f955ae67c278c7afb8ccdc58619b96672b38
wanquan <615861192@qq.com>
2021-04-09 19:13:46 +0800

    版本1.0.1

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ let serve = http.createServer();
 
 serve.on("request",function(request,response){
     let url = request.url;
-    console.log(url);
-    fs.readFile("./web/index.html",function(err,data){
-        response.writeHead(200,{'Content-Type': 'text/html','token':'wwwwwww-11111'})
-        response.end(data)
-    })
+    if(url == '/' || url.lastIndexOf(".html") > -1){//html页面
+        url = url == '/' ? '/index.html' : url;
+        fs.readFile(`./web${url}`,function(err,data){
+            response.writeHead(200,{'Content-Type': 'text/html','token':'wwwwwww-11111'})
+            response.end(data)
+        })
+    } else if(url){ //静态资源
+        fs.readFile(`./web${url}`,function(err,data){
+            response.end(data)
+        })
+    }
 })
 
 serve.listen(8080,function(){
